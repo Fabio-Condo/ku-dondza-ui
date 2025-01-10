8b9a09660810eb9ac97685edb56969c841c5732a
Fábio Condo <fabioenosse59@gmail.com>
2025-01-10 11:48:04 +0200

    update question quizz

--- src/app/quiz/questions/quizz-questions.component.ts
+++ src/app/quiz/questions/quizz-questions.component.ts
@@ -35,6 +35,10 @@ export class QuizzQuestionsComponent implements OnInit {
 
   correctAnswer: string | undefined; // Para armazenar a resposta correta como texto
 
+  showStartScreen: boolean = true;
+  showFinalScreen: boolean = false;
+
+
   @ViewChild('tabela') grid: any;
 
   filtro: QuestionFilter = {
@@ -160,8 +164,51 @@ export class QuizzQuestionsComponent implements OnInit {
     return userAnswer ? userAnswer.answerId === answerId : false;
   }
 
+  showFinalResults() {
+    this.showFinalScreen = true; 
+    this.calculateFinalResults(); 
+  }
+  
+  reviewQuestions() {
+    this.showFinalScreen = false; 
+    this.currentQuestionIndex = 0; 
+  }
+
+  restartQuiz() {
+    this.showFinalScreen = false; 
+    this.showStartScreen = true; 
+    this.userAnswers = []; 
+    this.currentQuestionIndex = 0; 
+  }
+
   toggleCorrection() {
     this.showCorrection = !this.showCorrection;
+    this.currentQuestionIndex = 0; // Volta para a primeira questão
+    this.showFinalScreen = false; // Oculta a tela final
+  }
+
+  // Novo método para iniciar o quiz
+  startQuiz() {
+    this.showStartScreen = false; // Oculta a tela inicial
+    this.currentQuestionIndex = 0; // Começa na primeira questão
+  }
+
+  // Método para calcular os resultados finais
+  calculateFinalResults() {
+    this.result.correctAnswers = 0;
+    this.result.incorrectAnswers = 0;
+
+    this.questions.forEach(question => {
+      const userAnswer = this.userAnswers.find(answer => answer.questionId === question.id);
+      if (userAnswer) {
+        const isCorrect = question.answers.some(answer => answer.id === userAnswer.answerId && answer.correct);
+        if (isCorrect) {
+          this.result.correctAnswers++;
+        } else {
+          this.result.incorrectAnswers++;
+        }
+      }
+    });
   }
 
   private sendErrorNotification(message: string): void {
