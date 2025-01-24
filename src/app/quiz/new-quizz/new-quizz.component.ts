9f54edffb5991c38d3190a5dfece38ae65b9bba1
Fábio Condo <fabioenosse59@gmail.com>
2025-01-24 12:01:42 +0200

    update results-summary

--- src/app/quiz/new-quizz/new-quizz.component.ts
+++ src/app/quiz/new-quizz/new-quizz.component.ts
@@ -18,7 +18,6 @@ import { Topic } from 'src/app/core/model/Topic';
 import { SubjectsService } from 'src/app/subjects/subjects.service';
 declare const MathJax: any;
 
-
 @Component({
   selector: 'app-new-quizz',
   templateUrl: './new-quizz.component.html',
@@ -37,11 +36,12 @@ export class NewQuizzComponent implements OnInit {
 
   imagePath = './assets/images/funcao do grau 2.png';
 
-  result: { correctAnswers: number; incorrectAnswers: number; unansweredQuestions: number } = {
-    correctAnswers: 0,
-    incorrectAnswers: 0,
-    unansweredQuestions: 0
-  };
+  result: {
+    correctAnswers: number;
+    incorrectAnswers: number;
+    nullAnswers: number; // Nova propriedade para respostas nulas
+  } = { correctAnswers: 0, incorrectAnswers: 0, nullAnswers: 0 };
+  
   showCorrection: boolean = false;
 
   showStartScreen: boolean = true;
@@ -80,17 +80,84 @@ export class NewQuizzComponent implements OnInit {
     this.scrollToTop();
   }
 
-  scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  // Método para calcular os resultados
+  calculateResults(): void {
+    this.result.correctAnswers = 0;
+    this.result.incorrectAnswers = 0;
+    this.result.nullAnswers = 0;
+
+    // Reinicia o objeto de resultados por tópico
+    this.quiz.resultsByTopic = {};
+
+    // Itera sobre todas as questões do quiz
+    this.quiz.questions.forEach((question) => {
+      const submittedAnswer = this.submittedAnswers.find(
+        (a) => a.question?.id === question.id
+      );
+
+      // Obtém o tópico da questão
+      const questionTopic = question.topic?.name || 'Sem tópico';
+
+      // Inicializa o tópico no objeto resultsByTopic, se necessário
+      if (!this.quiz.resultsByTopic[questionTopic]) {
+        this.quiz.resultsByTopic[questionTopic] = {
+          correct: 0,
+          incorrect: 0,
+          nullAnswers: 0,
+          total: 0,
+          percentage: 0
+        };
+      }
+
+      // Incrementa o total de questões por tópico
+      this.quiz.resultsByTopic[questionTopic].total++;
+
+      if (submittedAnswer) {
+        // Se o usuário respondeu, verifica se a resposta está correta ou incorreta
+        if (submittedAnswer.correct) {
+          this.result.correctAnswers++;
+          this.quiz.resultsByTopic[questionTopic].correct++;
+        } else {
+          this.result.incorrectAnswers++;
+          this.quiz.resultsByTopic[questionTopic].incorrect++;
+        }
+      } else {
+        // Se não há resposta submetida, conta como não respondida
+        this.result.nullAnswers++;
+        this.quiz.resultsByTopic[questionTopic].nullAnswers++;
+      }
+    });
+
+    // Calcula a porcentagem de acertos por tópico
+    for (const topic in this.quiz.resultsByTopic) {
+      const { correct, total } = this.quiz.resultsByTopic[topic];
+      this.quiz.resultsByTopic[topic].percentage = (correct / total) * 100;
+    }
   }
 
+  // Método para submeter as respostas
+  submitAnswers() {
+    // Calcula os resultados
+    this.calculateResults();
+
+    // Exibe a tela final
+    this.showFinalScreen = true;
+
+    // Salva o quiz, se ainda não foi submetido
+    if (!this.submited) {
+      this.saveQuiz();
+    }
+  }
+
+  // Método para iniciar o quiz
   startQuiz() {
     this.showStartScreen = false;
     this.currentQuestionIndex = 0;
-    this.renderMathExpressions(); // Renderiza as expressões matemáticas após carregar o quiz
+    this.renderMathExpressions();
     this.scrollToTop();
   }
 
+  // Método para carregar as disciplinas
   carregarDisciplinas() {
     this.subjectsService.findAll().subscribe({
       next: (dados) => {
@@ -102,6 +169,7 @@ export class NewQuizzComponent implements OnInit {
     });
   }
 
+  // Método para carregar os tópicos de uma disciplina
   getTopicsBySubjectId(subjectId: number): void {
     this.topicService.getSubjectsById(subjectId).subscribe(
       (dados: Topic[]) => {
@@ -116,6 +184,7 @@ export class NewQuizzComponent implements OnInit {
     );
   }
 
+  // Método para carregar as questões
   getQuestions(): void {
     const selectedTopicIds = this.getSelectedTopicIds();
 
@@ -125,7 +194,7 @@ export class NewQuizzComponent implements OnInit {
         this.questions = dados;
         this.quiz.questions = this.questions;
         this.showLoading = false;
-        this.renderMathExpressions(); // Renderiza as expressões matemáticas após carregar o quiz
+        this.renderMathExpressions();
       },
       (errorResponse: HttpErrorResponse) => {
         this.sendErrorNotification(errorResponse.error.message);
@@ -134,24 +203,23 @@ export class NewQuizzComponent implements OnInit {
     );
   }
 
+  // Método para alternar a seleção de um tópico
   toggleTopic(topic: Topic): void {
     topic.selected = !topic.selected;
   }
 
+  // Método para obter os tópicos selecionados
   getSelectedTopics(): Topic[] {
     return this.topics.filter(topic => topic.selected);
   }
 
+  // Método para obter os IDs dos tópicos selecionados
   getSelectedTopicIds(): number[] {
     return this.topics.filter(topic => topic.selected).map(topic => topic.id);
   }
 
-  submitSelectedTopics(): void {
-    const selectedTopics = this.getSelectedTopics();
-  }
-
+  // Método para salvar o quiz
   saveQuiz() {
-
     this.quiz.selectedTopics = this.getSelectedTopics();
 
     const topicIds = this.quiz.selectedTopics.map(topic => topic.id);
@@ -171,22 +239,7 @@ export class NewQuizzComponent implements OnInit {
     );
   }
 
-  submitAnswers() {
-    const totalQuestions = this.questions.length;
-    const answeredQuestions = this.submittedAnswers.filter(answer => answer.id !== -1).length;
-    const unansweredQuestions = totalQuestions - answeredQuestions;
-
-    this.result.correctAnswers = this.submittedAnswers.filter(answer => answer.correct).length;
-    this.result.incorrectAnswers = answeredQuestions - this.result.correctAnswers;
-    this.result.unansweredQuestions = unansweredQuestions;
-
-    if (!this.submited) {
-      this.saveQuiz();
-    }
-
-    this.showFinalScreen = true; // Mostra a tela final
-  }
-
+  // Método para capturar a resposta do usuário
   captureUserAnswer(questionId: number, answerId: number | null): void {
     const question = this.questions.find(q => q.id === questionId);
     if (question) {
@@ -224,30 +277,34 @@ export class NewQuizzComponent implements OnInit {
     }
   }
 
+  // Método para verificar se uma resposta foi selecionada
   isSelected(questionId: number, answerId: number): boolean {
     const userAnswer = this.submittedAnswers.find(a => a.question?.id === questionId);
     return userAnswer ? userAnswer.id === answerId : false;
   }
 
+  // Método para revisar as questões
   reviewQuestions() {
     this.showFinalScreen = false;
     this.currentQuestionIndex = 0;
     this.scrollToTop();
   }
 
+  // Método para iniciar um novo quiz
   newQuiz() {
     this.showFinalScreen = false;
     this.showStartScreen = true;
     this.submittedAnswers = [];
     this.currentQuestionIndex = 0;
-    this.result = { correctAnswers: 0, incorrectAnswers: 0, unansweredQuestions: 0 };
+    this.result = { correctAnswers: 0, incorrectAnswers: 0, nullAnswers: 0 };
   }
 
+  // Método para alternar a exibição da correção
   toggleCorrection() {
     this.showCorrection = true;
     this.currentQuestionIndex = 0;
     this.showFinalScreen = false;
-  
+
     // Aguarda a atualização do DOM antes de renderizar MathJax
     setTimeout(() => {
       this.renderMathExpressions();
@@ -256,18 +313,20 @@ export class NewQuizzComponent implements OnInit {
     this.scrollToTop();
   }
 
+  // Método para ir para a questão anterior
   goToPreviousQuestion() {
     if (this.currentQuestionIndex > 0) {
       this.currentQuestionIndex--;
-      this.renderMathExpressions(); // Renderiza as expressões matemáticas após carregar o quiz
+      this.renderMathExpressions();
       this.scrollToTop();
     }
   }
 
+  // Método para ir para a próxima questão
   goToNextQuestion() {
     if (this.currentQuestionIndex < this.questions.length - 1) {
       this.currentQuestionIndex++;
-      this.renderMathExpressions(); // Renderiza as expressões matemáticas após carregar o quiz
+      this.renderMathExpressions();
       this.scrollToTop();
     }
   }
@@ -290,6 +349,12 @@ export class NewQuizzComponent implements OnInit {
     }, 0);
   }
 
+  // Método para rolar a página para o topo
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  // Método para enviar notificações de erro
   private sendErrorNotification(message: string): void {
     if (message) {
       this.messageService.add({ severity: 'error', detail: message });
