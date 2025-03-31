a82b8dfd1dcc4a8ef502f5f6cf36104ca7a647e5
Fábio Condo <fabioenosse59@gmail.com>
2025-03-31 21:01:44 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent {
   title = 'post-app';
 
   showNavbar() {  
-    //return this.router.url !== '/login' && this.router.url !== '/home';
-    return this.router.url !== '/login';
+    return this.router.url !== '/login' && this.router.url !== '/home';
+    //return this.router.url !== '/login';
   }
 
   showFooter() {  
