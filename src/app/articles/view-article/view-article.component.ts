032b22b6e69fc5daf056862edba4419318463ef2
Fábio Condo <fabioenosse59@gmail.com>
2025-05-08 12:43:56 +0200

    update

--- src/app/articles/view-article/view-article.component.ts
+++ src/app/articles/view-article/view-article.component.ts
@@ -58,28 +58,28 @@ export class ViewArticleComponent implements OnInit {
     );
   }
 
-  //toggleLike(article: Article): void {
-  //  this.likeService.toggleLike(article.id).subscribe(
-  //    response => {
-  //      article.isLiked = !article.isLiked;
-  //      if (article.isLiked) {
-  //        article.numberOfLikes = article.numberOfLikes + 1;
-  //      } else {
-  //        article.numberOfLikes = article.numberOfLikes - 1;
-  //      }
-  //    },
-  //    (errorResponse: HttpErrorResponse) => {
-  //      this.sendErrorNotification(errorResponse.error.message);
-  //    }
-  //  );
-  //}
+  toggleLike(article: Article): void {
+    this.likeService.toggleLike(article.id).subscribe(
+      response => {
+        article.isLiked = !article.isLiked;
+        if (article.isLiked) {
+          article.numberOfLikes = article.numberOfLikes + 1;
+        } else {
+          article.numberOfLikes = article.numberOfLikes - 1;
+        }
+      },
+      (errorResponse: HttpErrorResponse) => {
+        this.sendErrorNotification(errorResponse.error.message);
+      }
+    );
+  }
 
   //addArticleToSavedArticles(article: Article): void {
   //  this.userService.addArticleToSavedArticles(this.loggedUser.id, article.id).subscribe(() => {
   //    article.isSaved = true;
   //  });
   //}
-  
+
   //removeArticleFromSavedArticles(article: Article): void {
   //  this.userService.removeArticleFromSavedArticles(this.loggedUser.id, article.id).subscribe(() => {
   //    article.isSaved = false;
