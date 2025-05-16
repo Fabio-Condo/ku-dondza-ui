ee5a991d4a3aa17d87175fbb24095652cad10a8c
Fábio Condo <fabioenosse59@gmail.com>
2025-05-16 15:13:59 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   //apiUrl: 'http://ku-dondza-lb-1715372122.us-east-1.elb.amazonaws.com'  
   //apiUrl: 'http://localhost:80'
-  apiUrl: 'http://localhost:8080',
-  //apiUrl: 'http://localhost:80',
+  //apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:81',
   //apiUrl: 'http://168.231.87.25:80',
   googleClientId: '170476897572-k758vjru9e2qqa707qhb5ns2kaaegquc.apps.googleusercontent.com' 
 
