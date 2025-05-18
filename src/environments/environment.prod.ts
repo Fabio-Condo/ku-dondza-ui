06a3fb15ab200cfbb1657ae91946da8c7497e7bb
Fábio Condo <fabioenosse59@gmail.com>
2025-05-18 11:44:51 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   //apiUrl: 'http://100.29.33.126:8080',
   //apiUrl: 'http://192.168.43.2:8080',
   //apiUrl: 'http://192.168.11.45:8080',
-  apiUrl: 'http://168.231.87.25:81',
+  apiUrl: 'http://168.231.87.25:80',
   googleClientId: '170476897572-k758vjru9e2qqa707qhb5ns2kaaegquc.apps.googleusercontent.com' 
 };
 
