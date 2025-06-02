e0719705a5f738178de678fbae8a72d3cb6ce46d
Fábio Condo <fabioenosse59@gmail.com>
2025-06-02 11:23:59 +0200

    update

--- src/app/main-panel/main-panel-routing.module.ts
+++ src/app/main-panel/main-panel-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { 
     path: 'main-panel', 
     component: MainPanelComponent,
-    canActivate: [AuthenticationGuard], 
+    //canActivate: [AuthenticationGuard], 
     //data: { 
     //  roles: ['ROLE_USER'], 
     //  requiresRoleCheck: true
