6a5cad8c915cf69bdad8bba67f8e3320147cb1d4
Fábio Condo <fabioenosse59@gmail.com>
2025-07-06 19:34:13 +0200

    update

--- src/app/topics/topic-view/topic-view.component.ts
+++ src/app/topics/topic-view/topic-view.component.ts
@@ -52,7 +52,6 @@ export class TopicViewComponent implements OnInit {
   }
 
   startQuestions() {
-    //this.router.navigate(['/questions', '575f8d9b-f924-42d3-9f23-944c6614653b']);
     this.router.navigate(['/questions', this.topic.questions[0].questionId]);
   }
 
