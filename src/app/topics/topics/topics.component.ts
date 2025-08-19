e95f18469b49bcd3dcad29f684849e6552dbd5cf
Fábio Condo <fabioenosse59@gmail.com>
2025-08-19 14:00:15 +0200

    update

--- src/app/topics/topics/topics.component.ts
+++ src/app/topics/topics/topics.component.ts
@@ -136,6 +136,7 @@ export class TopicsComponent implements OnInit {
 
   findAll(pagina: number = 0): void {
     this.loadingMessage = "Carregando dados"
+    this.loadingMessage = "Carregando dados"
     this.showLoading = true;
     this.filtro.pagina = this.currentPage - 1; // Ajuste para o padrão de paginação começando em 0
     this.topicService.filter(this.filtro).subscribe(
