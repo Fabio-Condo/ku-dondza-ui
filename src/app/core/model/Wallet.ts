e5b196f14a5fa1fb90aa7104662d460bb0d9cd83
Fábio Condo <fabioenosse59@gmail.com>
2025-10-26 17:12:35 +0200

    update

--- src/app/core/model/Wallet.ts
+++ src/app/core/model/Wallet.ts
@@ -5,12 +5,14 @@ export class Wallet {
     phoneNumber?: string;
     type?: string; // MPESA ou EMOLA
     default?: boolean;
+    active?: boolean;
     user = new User();
 
-    constructor(id?: number, phoneNumber?: string, type?: string, isDefault?: boolean) {
+    constructor(id?: number, phoneNumber?: string, type?: string, isDefault?: boolean, isActive?: boolean) {
         this.id = id;
         this.phoneNumber = phoneNumber;
         this.type = type;
         this.default = isDefault;
+        this.active = isActive;
     }
 }
