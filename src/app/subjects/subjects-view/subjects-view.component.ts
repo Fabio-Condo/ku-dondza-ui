d4ce2223bec53a86a331e683629301a06c45bd3b
Fábio Condo <fabioenosse59@gmail.com>
2025-10-27 18:56:22 +0200

    update

--- src/app/subjects/subjects-view/subjects-view.component.ts
+++ src/app/subjects/subjects-view/subjects-view.component.ts
@@ -19,6 +19,7 @@ import { UserService } from 'src/app/users/user.service';
 import { IUserFilter } from 'src/app/core/interface/IUserFilter';
 import { IApiResponse } from 'src/app/core/interface/IApiResponse';
 import { Question } from 'src/app/core/model/Question';
+import { Topic } from 'src/app/core/model/Topic';
 
 @Component({
   selector: 'app-subjects-view',
@@ -413,6 +414,21 @@ export class SubjectsViewComponent {
 
   onShowMoreStudents(): void {
     this.getStudentsBySubjectId(this.subject.id);
+  }
+
+  // bloqueia clique se o tópico Premium não estiver liberado para o usuário logado
+  isPremiumTopic(topic: Topic): boolean {
+    if (!topic.premium) return false;
+
+    // desabilita se não estiver logado ou se estiver no plano FREE
+    return this.isFreeUser();
+  }
+
+  isFreeUser(): boolean {
+    if (!this.loggedUser || this.loggedUser.id === 0) return true;
+
+    const expiresAt = this.loggedUser.expiresAt ? new Date(this.loggedUser.expiresAt) : null;
+    return this.loggedUser.plan === 'FREE' || !expiresAt || expiresAt <= new Date();
   }
 
   public get isAdmin(): boolean {
