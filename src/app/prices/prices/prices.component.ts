266fbb4eae48772ca45dbf3ded531fe0d7a87a0f
Fábio Condo <fabioenosse59@gmail.com>
2025-12-24 09:39:30 +0200

    update

--- src/app/prices/prices/prices.component.ts
+++ src/app/prices/prices/prices.component.ts
@@ -160,6 +160,12 @@ export class PricesComponent implements OnInit {
   }
 
   private async initializeGoogleAuth(): Promise<void> {
+
+    if (this.isMobile()) {
+      console.log('Mobile detected — Google Auth skipped.');
+      return;
+    }
+
     try {
       const setupButton = await this.googleAuthService.initializeGoogleButton('google-signin-button');
       setupButton((credential) => this.handleGoogleCredential(credential));
@@ -202,6 +208,11 @@ export class PricesComponent implements OnInit {
     });
 
     this.subscriptions.push(sub);
+  }
+
+  public isMobile(): boolean {
+    //return true;
+    return /android|iphone|ipad|ipod/i.test(navigator.userAgent);
   }
 
   setActiveTab(tabIndex: number) {
