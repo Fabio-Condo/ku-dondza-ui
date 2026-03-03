c146106f04e20e2bd29268e3dbdcdefd4983229b
Fábio Condo <fabioenosse59@gmail.com>
2026-03-03 09:47:22 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,11 @@ import { NaoAutorizadoComponent } from './core/nao-autorizado.component';
 
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 import { HomeComponent } from './home/home/home.component';
-import { MainPanelComponent } from './main-panel/main-panel/main-panel.component';
 
 const routes: Routes = [
   { path: 'nao-autorizado', component: NaoAutorizadoComponent },
-  { path: 'main-panel', component: MainPanelComponent },
-  { path: '', redirectTo: 'main-panel', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
   { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ];
