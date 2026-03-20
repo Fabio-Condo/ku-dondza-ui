f43de846a046f3d65f2e64501fa3403294834eeb
Fábio Condo <fabioenosse59@gmail.com>
2026-03-20 09:28:37 +0200

    update

--- src/app/exames/exames/exames.component.ts
+++ src/app/exames/exames/exames.component.ts
@@ -317,8 +317,11 @@ export class ExamesComponent implements OnInit {
   isPremiumExam(exam: Exam): boolean {
     if (!exam.premium) return false;
 
+    // ADMIN sempre tem acesso
+    if (this.loggedUser && this.isAdmin) return false;
+
     // desabilita se não estiver logado ou se estiver no plano FREE
-    return this.isFreeUser();
+    return this.isFreeUser();``
   }
 
   isFreeUser(): boolean {
