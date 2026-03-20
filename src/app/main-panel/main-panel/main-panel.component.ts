1ef36f399db48612c33aa3fd47ce5c3d98ed24b2
Fábio Condo <fabioenosse59@gmail.com>
2026-03-20 10:43:11 +0200

    update

--- src/app/main-panel/main-panel/main-panel.component.ts
+++ src/app/main-panel/main-panel/main-panel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { User } from 'src/app/core/model/User';
+import { Role } from 'src/app/enum/role.enum';
+import { AuthenticationService } from 'src/app/users/authentication.service';
 
 
 @Component({
@@ -7,19 +10,37 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './main-panel.component.html',
   styleUrls: ['./main-panel.component.css']
 })
-export class MainPanelComponent  implements OnInit {
+export class MainPanelComponent implements OnInit {
+
+  loggedUser: User = new User();
+  isUserLoggedIn: boolean = false;
 
   constructor(
     private title: Title,
+    private authenticationService: AuthenticationService,
   ) { }
 
   ngOnInit(): void {
     this.title.setTitle('Main painel page');
+    this.isUserLoggedIn = this.authenticationService.isUserLoggedIn();
+    this.loggedUser = this.authenticationService.getUserFromLocalCache();
     this.scrollToTop();
   }
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  public get isAdmin(): boolean {
+    return this.getUserRole() === Role.ADMIN || this.getUserRole() === Role.SUPER_ADMIN;
+  }
+
+  public get isSuperAdmin(): boolean {
+    return this.getUserRole() === Role.SUPER_ADMIN;
+  }
+
+  private getUserRole(): string {
+    return this.authenticationService.getUserFromLocalCache().role;
   }
 
 }
