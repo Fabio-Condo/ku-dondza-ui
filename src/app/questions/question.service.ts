76648b885516f1b58245cc69da8cd9fc11f558fd
Fábio Condo <fabioenosse59@gmail.com>
2026-03-27 09:09:35 +0200

    update

--- src/app/questions/question.service.ts
+++ src/app/questions/question.service.ts
@@ -21,7 +21,8 @@ export class QuestionService {
   private questionsCache = new Map<string, CacheEntry<IApiResponse<Question>>>();
   private questionCache = new Map<string, CacheEntry<Question>>();
 
-  private CACHE_TTL = 10 * 60 * 1000; // 5 minutos
+  //private CACHE_TTL = 10 * 60 * 1000; // 5 minutos
+  private CACHE_TTL = 1000 * 60 * 60 * 24; // 24h
 
   private isCacheValid(entry: CacheEntry<any>): boolean {
     return (Date.now() - entry.timestamp) < this.CACHE_TTL;
