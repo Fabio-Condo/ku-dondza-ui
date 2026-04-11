e4292d87f1589b6d56682c66321b59c081749c09
Fábio Condo <fabioenosse59@gmail.com>
2026-04-11 13:52:00 +0200

    update

--- src/app/subjects/subjects/subjects.component.ts
+++ src/app/subjects/subjects/subjects.component.ts
@@ -160,8 +160,12 @@ export class SubjectsComponent implements OnInit {
       )
     ).subscribe(
       (dados: IApiResponse<Subject>) => {
-        //this.subjects = dados.content;
-        this.subjects = dados.content.filter(subject => subject.courseEnabled);
+        if (this.isSuperAdmin) {
+          this.subjects = dados.content;
+        }else {
+          this.subjects = dados.content.filter(subject => subject.courseEnabled);
+        }
+
         this.totalRecords = dados.totalElements;
         if (this.totalSubjects == 0) {
           this.totalSubjects = dados.totalElements;
