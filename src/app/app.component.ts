47be8723d47405018b267d79a3bb94c41e033a2a
Fábio Condo <fabioenosse59@gmail.com>
2026-05-27 11:45:47 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,6 +45,6 @@ export class AppComponent {
 
   showMobileFooter() {
     return this.router.url == '/quizzes' || this.router.url == '/main-panel' || this.router.url == '/questions' || this.router.url == '/topics' || this.router.url == '/subjects'
-      || this.router.url == '/exames' || this.router.url == '/progress' || this.router.url == '/users';
+      || this.router.url == '/exames' || this.router.url == '/progress' || this.router.url == '/challenges' || this.router.url == '/users';
   }
 }
