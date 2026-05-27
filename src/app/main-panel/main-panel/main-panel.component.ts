46334dfba21a71ccab23196be66d1bfc7c1361c6
Fábio Condo <fabioenosse59@gmail.com>
2026-05-27 16:19:01 +0200

    update

--- src/app/main-panel/main-panel/main-panel.component.ts
+++ src/app/main-panel/main-panel/main-panel.component.ts
@@ -136,7 +136,7 @@ export class MainPanelComponent implements OnInit {
     //  this.getUserProgress();
     //}
     this.getUserProgress();
-    this.getQuestions(0);
+    this.getQuestions();
     this.getCourses();
     this.getExames();
     this.getQuizzes();
@@ -710,6 +710,7 @@ export class MainPanelComponent implements OnInit {
 
     // Já submeteu
     if (challenge.hasCurrentUserSubmitted === true) {
+      this.sendErrorNotification("Já identificamos que você submeteu este desafio. Aguarde ele finalizar para ver os resultados!");
       return;
     }
 
