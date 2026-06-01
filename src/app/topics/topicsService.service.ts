b934e67aa34e97992ecf0c0a9a20105f0dc4859c
Fábio Condo <fabioenosse59@gmail.com>
2026-06-01 16:01:30 +0200

    update

--- src/app/topics/topicsService.service.ts
+++ src/app/topics/topicsService.service.ts
@@ -118,10 +118,6 @@ export class TopicService {
         return this.http.get<Topic[]>(`${this.host}/subjects/${subjectId}`);
     }
 
-    //getTopicByTopicId(topicId: string): Observable<Topic> {
-    //    return this.http.get<Topic>(`${this.host}/find-by-topicId/${topicId}`, {});
-    //}
-
     add(topic: Topic): Observable<Topic> {
         return this.http.post<Topic>(this.host, topic).pipe(
             tap(() => this.clearCache())
